1f3edd4015d4ec008a5fd3a631879bced74930bb
Gabriel Michel <student46@Gabriel-M.local>
2022-05-02 16:13:09 -0400

    made a comment

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import './NavBar.css'
+
+function Navbar(){
+    return (
+       <div className="navBar">
+           <ul>
+               <li>Home</li>
+               <li>Contacts</li>
+               <li>About</li>
+           </ul>
+       </div> 
+    )
+}
+
+export default Navbar;
